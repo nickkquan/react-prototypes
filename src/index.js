f0123d05ee73cfe63f9d72ee77ff89fa33992956
Nick Quan <nickkquan@gmail.com>
2018-01-19 10:03:35 -0800

    completed reacted protoype intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const luckyNumber = () => Math.ceil(Math.random() * 1000);
+
+const Greeting = user => {
+  let domElement = (
+    <div className="container">
+      <h1>Greetings! {user}</h1>
+      <h2 className="text-muted">Your lucky number is: {luckyNumber()}</h2>
+    </div>
+  );
+  return domElement;
+};
+
+// const user = {
+//   name: "Nick",
+//   luckyNumber: luckyNumber()
+// };
+
+class User {
+  constructor(name) {
+    this.name = name;
+    this.luckyNumber = luckyNumber();
+  }
+}
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
+  //   <h1 className="container">React Prototypes!</h1>,
+  Greeting(new User("Nick").name),
+  document.getElementById("root")
 );
