a269568a933374e16e58097dba2b380c4d6f4c4c
Nick Quan <nickkquan@gmail.com>
2018-01-23 09:34:43 -0800

    functional-components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from "react";
+import Table from "./table";
 
+const students = [
+  {
+    student: "Hermione Granger",
+    course: "Muggle Studies",
+    grade: 95
+  },
+  {
+    student: "Harry Potter",
+    course: "Defense Against the Dark Arts",
+    grade: 95
+  },
+  {
+    student: "Neville Longbottom",
+    course: "Herbology",
+    grade: 95
+  }
+];
 
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Student Grade Table</h1>
+      <Table data={students} />
+    </div>
+  );
+};
+
+export default App;
