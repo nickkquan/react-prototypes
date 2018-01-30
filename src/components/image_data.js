6fdff9ae766fb88fa9ade64995ecde3352ddf776
Nick Quan <nickkquan@gmail.com>
2018-01-30 14:07:27 -0800

    styles-images completed

---
+++ src/components/image_data.js
@@ -0,0 +1,20 @@
+import React from "react";
+import space1 from "./images/nasa.png";
+import space2 from "./images/carina.jpg";
+import space3 from "./images/crab.jpg";
+import space4 from "./images/earth.jpg";
+import space5 from "./images/jupiter.jpg";
+import space6 from "./images/moon.jpg";
+import space7 from "./images/orion.jpg";
+import space8 from "./images/saturn.jpg";
+
+export default [
+  { src: space1, style: { left: "5%", transform: "rotate(-10deg)" } },
+  { src: space2, style: { left: "40%", transform: "rotate(6deg)" } },
+  { src: space3, style: { right: "15%", transform: "rotate(-9deg)" } },
+  { src: space4, style: { left: "8%", top: "28%", transform: "rotate(8deg)" } },
+  { src: space5, style: { left: "25%", top: "48%", transform: "rotate(-5deg)" } },
+  { src: space6, style: { right: "12%", top: "26%", transform: "rotate(10deg)" } },
+  { src: space7, style: { left: "25%", top: "48%", transform: "rotate(5deg)" } },
+  { src: space8, style: { right: "25%", top: "25%", transform: "rotate(16deg)" } }
+];
---
+++ src/components/scrapbook_images.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => {
+  console.log(props);
+  const { src, style } = props.about;
+  return <img className="scrapbook-image" src={src} style={style} />;
+};
